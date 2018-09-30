9d420d468cd2d0694e0b0ec2903ff9b7ea55abe0
Rick <rick.smit@ahold.com>
2018-09-30 17:36:40 +0200

    added prettier to avoid doublequotes

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,88 +1,88 @@
-import { compileFileSync } from "bsb-js";
+import { compileFileSync } from 'bsb-js'
 
-import fs from "fs";
-import { getPathForComponent } from "./utils";
-import path from "path";
+import fs from 'fs'
+import { getPathForComponent } from './utils'
+import path from 'path'
 
-const BS_TEST = /\.(bs.js)$/;
-const REASON_TEST = /\.(ml|re)$/;
+const BS_TEST = /\.(bs.js)$/
+const REASON_TEST = /\.(ml|re)$/
 
-const isCompiledFile = fileName => BS_TEST.test(fileName);
-const isReasonFile = fileName => REASON_TEST.test(fileName);
+const isCompiledFile = fileName => BS_TEST.test(fileName)
+const isReasonFile = fileName => REASON_TEST.test(fileName)
 
 export const onCreateWebpackConfig = ({ stage, actions }) => {
-  const { setWebpackConfig } = actions;
+  const { setWebpackConfig } = actions
   setWebpackConfig({
     module: {
       rules: [
         {
           test: REASON_TEST,
-          use: [`bs-loader`]
-        }
-      ]
-    }
-  });
-};
+          use: [`bs-loader`],
+        },
+      ],
+    },
+  })
+}
 
 const jsFilePath = (buildDir, moduleDir, resourcePath, inSource, bsSuffix) => {
-  const mlFileName = resourcePath.replace(buildDir, "");
-  const jsFileName = mlFileName.replace(REASON_TEST, bsSuffix);
+  const mlFileName = resourcePath.replace(buildDir, '')
+  const jsFileName = mlFileName.replace(REASON_TEST, bsSuffix)
 
   if (inSource) {
-    return path.join(buildDir, jsFileName);
+    return path.join(buildDir, jsFileName)
   }
 
-  return path.join(buildDir, "lib", moduleDir, jsFileName);
-};
+  return path.join(buildDir, 'lib', moduleDir, jsFileName)
+}
 
 export const preprocessSource = ({ filename }) => {
   if (!isReasonFile(filename)) {
-    return null;
+    return null
   }
-  const moduleDir = "js";
+  const moduleDir = 'js'
   const compiledFilePath = jsFilePath(
     process.cwd(),
     moduleDir,
     filename,
     false,
-    ".bs.js"
-  );
+    '.bs.js'
+  )
   try {
-    return compileFileSync(moduleDir, compiledFilePath);
+    return compileFileSync(moduleDir, compiledFilePath)
   } catch (e) {
     // Don't need to print error message since bsb will already do that
   }
-};
+}
 
-export const resolvableExtensions = () => [".ml", ".re"];
+export const resolvableExtensions = () => ['.ml', '.re']
 
 export const onCreatePage = (
   { page, boundActionCreators: { createPage, deletePage } },
   { derivePathFromComponentName }
 ) => {
   return new Promise((resolve, reject) => {
-    const oldPage = Object.assign({}, page);
-    const { component, path } = page;
+    const oldPage = Object.assign({}, page)
+    const { component, path } = page
 
     if (isCompiledFile(component)) {
       // Remove .bs components so we don't have duplicates
-      deletePage(oldPage);
+      deletePage(oldPage)
     } else if (
       derivePathFromComponentName &&
       isReasonFile(component) &&
-      !path.endsWith(".html")
+      !path.endsWith('.html')
     ) {
       // Try to grab the name of the component from the ReasonReact
       // component instead of using the file name
-      const source = fs.readFileSync(component, "utf-8");
-      const newPath = getPathForComponent(source);
+      const source = fs.readFileSync(component, 'utf-8')
+      const newPath = getPathForComponent(source)
       if (newPath !== undefined) {
-        const newPage = Object.assign({}, page, { path: newPath });
-        deletePage(oldPage);
-        createPage(newPage);
+        const newPage = Object.assign({}, page, { path: newPath })
+        deletePage(oldPage)
+        createPage(newPage)
       }
     }
 
-    resolve();
-  });
-};
+    resolve()
+  })
+}
