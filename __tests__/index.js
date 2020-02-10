37f2a18015ae30a8881c807f1cb0d65a0a20fe47
Denis Strelkov <ds@planado.ru>
2020-02-10 23:19:20 +0700

    Refactor tests and utils

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,26 +1,29 @@
-import { getComponentDefinitionLine, getNameFromReasonComponent } from '../src/utils';
+import fs from 'fs'
+import path from 'path'
+
+import { getLineWithComponentName, extractNameFromLine } from '../src/utils'
+
+const fixturePath = filename => path.join(__dirname, 'fixtures', filename)
+
+const readFixture = filename => fs.readFileSync(fixturePath(filename), 'utf-8')
 
 describe('gatsby-plugin-reason', () => {
     describe('Paths', () => {
-        const fileContents = `
-            let component = ReasonReact.reducerComponent("MyComponent");
-
-            let my_var = "stuff";
-
-            let x = 1 + 2;
-        `;
+        const fileContents = readFixture('reducerComponent')
 
         test('Finds the line in the component file that contains the name', () => {
-            const actual = getComponentDefinitionLine(fileContents);
-            const expected = 'let component = ReasonReact.reducerComponent("MyComponent");';
-            expect(actual).toEqual(expect.stringContaining(expected));
-        });
+            const line = getLineWithComponentName(fileContents)
+            const expected =
+                'let component = ReasonReact.reducerComponent("MyComponent");'
+
+            expect(line).toEqual(expected)
+        })
 
         test('Parses the name from the name of the ReasonReact component', () => {
-            const line = getComponentDefinitionLine(fileContents);
-            const actual = getNameFromReasonComponent(line);
-            const expected = 'MyComponent';
-            expect(actual).toEqual(expected);
-        });
-    });
-});
+            const line = getLineWithComponentName(fileContents)
+            const name = extractNameFromLine(line)
+
+            expect(name).toEqual('MyComponent')
+        })
+    })
+})
