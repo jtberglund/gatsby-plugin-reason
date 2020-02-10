b48d1ab0202430261affe8680157b033e9f85617
Denis Strelkov <ds@planado.ru>
2020-02-11 00:46:52 +0700

    Extract name for functional component

--- __tests__/index.js
+++ __tests__/index.js
@@ -27,14 +27,37 @@ context('Find the line in the component file that contains the name', () => {
             'let component = ReasonReact.reducerComponent("MyComponent");'
         )
     })
+
+    test('File with functional component', () => {
+        const line = findLineIn('functionalComponent')
+        expect(line).toEqual(
+            'React.setDisplayName(make, "FunctionalComponentName");'
+        )
+    })
 })
 
 context('Extract the page name from line with code', () => {
-    it('works', () => {
-        const fileContents = readFixture('reducerComponent')
-        const line = getLineWithComponentName(fileContents)
-        const name = extractNameFromLine(line)
+    context('Old record-style components', () => {
+        const components = [
+            'statelessComponent',
+            'statelessComponentWithRetainedProps',
+            'reducerComponent',
+            'reducerComponentWithRetainedProps',
+        ]
 
-        expect(name).toEqual('MyComponent')
+        components.forEach(c => {
+            const line = `let component = ReasonReact.${c}("MyComponent");`
+
+            it(`extracts name from ${c}`, () => {
+                const name = extractNameFromLine(line)
+                expect(name).toEqual('MyComponent')
+            })
+        })
+    })
+
+    it(`extracts name from line with name for functional component`, () => {
+        const line = 'React.setDisplayName(make, "FunctionalComponentName");'
+        const name = extractNameFromLine(line)
+        expect(name).toEqual('FunctionalComponentName')
     })
 })
