0c4e181c28dd0346d65b0cb2daed71e22a7179a2
Denis Strelkov <ds@planado.ru>
2020-02-10 21:17:55 +0700

    Update babel and jest deps

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,16 @@
 module.exports = {
     presets: [
-        ['@babel/env', {
-            targets: {
-                node: 8,
+        [
+            '@babel/preset-env',
+            {
+                targets: {
+                    node: 8,
+                },
+                corejs: 3,
+                loose: true,
+                useBuiltIns: 'usage',
+                shippedProposals: true,
             },
-            loose: true,
-            useBuiltIns: 'usage',
-            shippedProposals: true,
-        }],
+        ],
     ],
 }
